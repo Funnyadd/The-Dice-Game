cc1ea9a24d58b481ac12c3b2e09f2507a0ecbd0d
Adam Mihajlovic <mihajlovicadam@gmail.com>
2026-03-08 00:05:09 -0500

    added more notes

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -5,10 +5,15 @@ const Board = ({ tiles, onTileClick }: BoardProps) => {
     // Need to find a way to make the tiles be higher than the height of the board
     // so that it gives an effect that they are over the edge
 
-    // Replace disabled state by a giggle
+    // Find a way to resize the board based on the viewport
 
-    // Add some sort of animation (if possible) from state up to selected
+    // Find a way to make the board game work well in vertical
+    // (buttons might be too small at the moment)
+    // Horizontal phone viewport in browsers is horrible :(
 
+    // TILES:
+    // - Replace disabled state by a giggle
+    // - Add some sort of animation (if possible) from state up to selected
     return (
         <div className="board-container">
             <div className="tile-container">
